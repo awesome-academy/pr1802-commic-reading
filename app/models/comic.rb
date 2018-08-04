020b9a7ff283ccddb701eb109289be87eb27d3db
VuongGiang <vuonggiang.bkhn@gmail.com>
2018-08-04 19:21:48 +0700

    Create Layout

--- app/models/comic.rb
+++ app/models/comic.rb
@@ -6,4 +6,8 @@ class Comic < ApplicationRecord
   has_many :chapters
   has_many :comic_categories
   has_many :categories, through: :comic_categories
+
+  validates :user_id, presence: true
+  validates :title, presence: true
+  validates :content, presence: true
 end
