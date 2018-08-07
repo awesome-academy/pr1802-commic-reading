bc9ab81bbb44bd35649efe149991d13c5fe59866
LinhVu <linhvu231@gmail.com>
2018-08-08 00:02:25 +0700

    login

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180803122115) do
+ActiveRecord::Schema.define(version: 20180807163134) do
 
   create_table "authors", force: :cascade do |t|
     t.string "name"
@@ -88,6 +88,8 @@ ActiveRecord::Schema.define(version: 20180803122115) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
+    t.string "remember_digest"
   end
 
 end
